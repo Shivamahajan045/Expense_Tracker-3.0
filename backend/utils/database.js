f86f134c8344f3d89f88c01b59231e3bcec4cc6e
shivakumar <shivamahajan055@gmail.com>
2025-05-01 12:34:32 +0530

    updated database.js

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -8,9 +8,7 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    logging: false,
   }
 );
 
-// console.log(process.nextTi)
 module.exports = sequelize;
