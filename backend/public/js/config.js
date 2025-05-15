12fc3447206a557ac73799eaf2d9c0a74702e8f7
shivakumar <shivamahajan055@gmail.com>
2025-05-15 11:23:38 +0530

    removed port

--- backend/public/js/config.js
+++ backend/public/js/config.js
@@ -3,8 +3,6 @@ const isLocalhost =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1";
 
-const BASE_URL = isLocalhost
-  ? "http://localhost:3000"
-  : "http://52.66.66.94:3000"; //remove port once using nginx
+const BASE_URL = isLocalhost ? "http://localhost:3000" : "http://52.66.66.94"; //remove port once using nginx
 
 window.BASE_URL = BASE_URL;
